779571466e5ec054c48ce1b05789b924796d6a77
Manty-S-Djusaem <104922578+Manty-S-Djusaem@users.noreply.github.com>
2023-07-31 02:06:07 +0600

    almost done page

--- src/ui/Accordion.jsx
+++ src/ui/Accordion.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import module from './Accordion.module.scss';
 
 const Accordion = () => {
     const [activeIndices, setActiveIndices] = useState([]);
 
+    useEffect(() => {
+        const savedIndices = localStorage.getItem('activeIndices');
+        if (savedIndices) {
+            setActiveIndices(JSON.parse(savedIndices));
+        }
+    }, []);
+
     const handleAccordionClick = (index) => {
-        // Check if the clicked index is already active
         if (activeIndices.includes(index)) {
-            // If active, remove it from the activeIndices array
             setActiveIndices(activeIndices.filter((i) => i !== index));
         } else {
-            // If not active, add it to the activeIndices array
             setActiveIndices([...activeIndices, index]);
         }
     };
+
+    useEffect(() => {
+        localStorage.setItem('activeIndices', JSON.stringify(activeIndices));
+    }, [activeIndices]);
 
     const accordionData = [
         {
@@ -22,17 +30,17 @@ const Accordion = () => {
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         },
         {
-            title: 'Вопрос 1',
+            title: 'Вопрос 2',
             content:
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         },
         {
-            title: 'Вопрос 1',
+            title: 'Вопрос 3',
             content:
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         },
         {
-            title: 'Вопрос 1',
+            title: 'Вопрос 4',
             content:
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         },
@@ -44,13 +52,19 @@ const Accordion = () => {
             {accordionData.map((item, index) => (
                 <div key={index} className={module.accordion}>
                     <button
-                        className={`${module.accordionbtn} ${activeIndices.includes(index) ? module.active : ''}`}
+                        className={`${module.accordionbtn} ${activeIndices.includes(index) ? module.active : ''
+                            }`}
                         onClick={() => handleAccordionClick(index)}
                     >
                         {item.title}
-                        <span className={module.arrow}>{activeIndices.includes(index) ? 'ᐱ' : 'ᐯ'}</span>
+                        <span className={module.arrow}>
+                            {activeIndices.includes(index) ? 'ᐱ' : 'ᐯ'}
+                        </span>
                     </button>
-                    <div className={`${module.panel} ${activeIndices.includes(index) ? module.active : ''}`}>
+                    <div
+                        className={`${module.panel} ${activeIndices.includes(index) ? module.active : ''
+                            }`}
+                    >
                         <p className={module.panel_content}>{item.content}</p>
                     </div>
                 </div>
